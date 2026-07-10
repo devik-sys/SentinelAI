1e347060a1e526ac870b589bfc7f1661b84a1267
Devi Krishna Manoj <devikrishna251105@gmail.com>
2026-07-10 22:02:29 +0530

    Integrated ArcFace recognition and optimized inference

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import cv2
 from camera import get_frame, release_camera
 from ai_recognition import recognize
 
+FRAME_SKIP = 15
+
+frame_count = 0
+
+last_name = "Unknown"
+
 while True:
 
     frame = get_frame()
@@ -10,17 +16,28 @@ while True:
     if frame is None:
         break
 
-    # AI Recognition
-    name = recognize(frame)
+    frame_count += 1
 
-    # Display Name
+    # Run AI only every 15 frames
+    if frame_count % FRAME_SKIP == 0:
+
+        print("Running AI Recognition...")
+
+        last_name = recognize(frame)
+
+    # Display last recognized name
+    color = (0, 255, 0)
+
+    if last_name == "Unknown":
+        color = (0, 0, 255)
+
     cv2.putText(
         frame,
-        name,
+        last_name,
         (40, 50),
         cv2.FONT_HERSHEY_SIMPLEX,
         1,
-        (0, 255, 0),
+        color,
         2
     )
 
