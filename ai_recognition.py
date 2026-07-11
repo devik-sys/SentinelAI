f82359f24dd91d5885cc0617d4f2694adba1100c
Devi Krishna Manoj <devikrishna251105@gmail.com>
2026-07-11 15:07:25 +0530

    Improved ArcFace recognition and reduced false positives

--- ai_recognition.py
+++ ai_recognition.py
@@ -1,13 +1,38 @@
 from deepface import DeepFace
+import cv2
 import os
+
+# Load OpenCV face detector
+face_detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    "haarcascade_frontalface_default.xml"
+)
 
 
 def recognize(frame):
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_detector.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(120,120)
+    )
+
+    # No face
+    if len(faces) == 0:
+        return "No Face"
+
+    # Largest detected face
+    x, y, w, h = max(faces, key=lambda f: f[2] * f[3])
+
+    face = frame[y:y+h, x:x+w]
+
     try:
 
         result = DeepFace.find(
-            img_path=frame,
+            img_path=face,
             db_path="faces",
             model_name="ArcFace",
             enforce_detection=False,
@@ -16,17 +41,27 @@ def recognize(frame):
 
         if len(result) > 0 and len(result[0]) > 0:
 
-            identity = result[0].iloc[0]["identity"]
+            best = result[0].iloc[0]
+
+            identity = best["identity"]
+
+            distance = best["distance"]
+
+            print(f"Match: {identity}")
+            print(f"Distance: {distance:.4f}")
 
             name = os.path.basename(
                 os.path.dirname(identity)
             )
 
-            return name
+            if distance < 0.35:
+                print("✅ Recognized:", name)
+                return name
+
+            print("❌ Unknown")
+            return "Unknown"
 
         return "Unknown"
 
-    except Exception as e:
-
-        print("Recognition Error:", e)
+    except Exception:
         return "Unknown"
